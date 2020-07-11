5828d2d2983eda2a5213634ffd374b9843affedd
Veronika <nebesnav@gmail.com>
2020-07-12 00:30:36 +0300

    add burger menu

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
-let btnUp = document.querySelector('.arrow_footer');
-console.log(btnUp);
+// let btnUp = document.querySelector('.arrow_footer');
+// // console.log(btnUp);
 
-btnUp.addEventListener("click",function(){
-    window.scrollTo(0,0);
+// btnUp.addEventListener("click",function(){
+//     window.scrollTo(0,0);
     
+// })
+
+
+
+// --------Прокручування сторінки вверх -------
+
+$(".arrow_footer").click(function(){
+  $("body,html").animate({
+    scrollTop:0
+  },1000)
 })
+
 
 // $(".slider_foto_dog").slick({
 //     dots:true,
@@ -25,6 +36,31 @@ btnUp.addEventListener("click",function(){
 
 
 //   });
-
+// --------Анімація для блоків-------
 
   AOS.init();
+
+
+// --------------------- Мобільне меню ------------
+  
+  let burgerMenu = document.querySelector(".mobile-menu");
+
+  let showMenu = document.querySelector(".navigation");
+
+  burgerMenu.addEventListener("click", function(){
+    burgerMenu.classList.toggle("active_menu")
+    if(burgerMenu.classList.contains("active_menu")){
+      showMenu.classList.add("active_menu");
+    }else{
+      showMenu.classList.remove("active_menu")
+    }
+  })
+
+  let closeBtnMobileMenu = document.querySelector(".close");
+  // console.log(closeBtnMobileMenu)
+  closeBtnMobileMenu.addEventListener("click",function(){
+    
+      showMenu.classList.remove("active_menu");
+      burgerMenu.classList.toggle("active_menu")
+    
+  })
